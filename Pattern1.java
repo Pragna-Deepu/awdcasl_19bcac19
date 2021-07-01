7764871ecd18a7a32e8fac2e55dbe315d79ed231
Pragna-Deepu <86730768+Pragna-Deepu@users.noreply.github.com>
2021-07-01 18:35:53 +0530

    Update Pattern1.java

--- Pattern1.java
+++ Pattern1.java
@@ -1,18 +1,20 @@
 import java.util.*;
-public class Pattern1
+class Pattern1
 {
 public static void main(String args[])
 {
-Scanner sc = new Scanner(System.in);
+Scanner sc=new Scanner(System.in);
 System.out.println("enter the number of rows");
-int rows = sc.nextInt();
-for (int i= rows-1; i>=0 ; i--)
+int rows=sc.nextInt();
+for(int i=rows-1;i>=0;i--)
 {
-for (int j=0; j<=i; j++)
+for(int j=0;j<=i;j++)
 {
-System.out.print("*" + " ");
+System.out.print("* ");
 }
 System.out.println();
 }
 }
 }
+
+
