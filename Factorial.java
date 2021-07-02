28bcc8e14846af24c0b4a7a8e5f96b8286da8468
Pragna-Deepu <86730768+Pragna-Deepu@users.noreply.github.com>
2021-07-02 19:27:01 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,17 +1,29 @@
 import java.util.*;
 class Factorial
 {
+static void count(int n)
+{
+for(int k=1;k<=n;k++)
+{
+Scanner s=new Scanner(System.in);
+int num=s.nextInt();
+}
+}
 public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
 System.out.println("enter a num: ");
 int n=sc.nextInt();
-int i=1,product=1;
-while(i<=n)
+count(n);
+int i=1,product=1,num;
+for(int j=1;j<=n;j++)
+{
+while(i<=num)
 {
 product=product*i;
 i++;
 }
 System.out.println("the factorial of the num is: "+product);
+}
 }
 }
