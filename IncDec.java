4c95589346c36fc7ae3cce0c8e6878461a1b06dc
Pragna-Deepu <86730768+Pragna-Deepu@users.noreply.github.com>
2021-07-02 18:28:06 +0530

    Update IncDec.java

--- IncDec.java
+++ IncDec.java
@@ -5,14 +5,14 @@ public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
 System.out.println("enter a number: ");
-int N=sc.nextInt();
-if(N%4==0)
+int n=sc.nextInt();
+if(n%4==0)
 {
-System.out.println("the num is divisible,after inc: "+(++N));
+System.out.println("the num is divisible,after inc: "+(++n));
 }
 else
 {
-System.out.println("the num is not divisible,after dec "+(--N));
+System.out.println("the num is not divisible,after dec "+(--n));
 }
 }
 }
