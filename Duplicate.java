2e04535d35770cc136cc81bfa83fd749a40e7de8
Pragna-Deepu <86730768+Pragna-Deepu@users.noreply.github.com>
2021-07-05 20:27:18 +0530

    Add files via upload

---
+++ Duplicate.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Duplicate
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println(" enter the number: ");
+int n=sc.nextInt();
+
+int i,j;
+int arr[]=new int[n];
+for( i=0;i<n;i++)
+{
+arr[i]=sc.nextInt();
+}
+for(i=0;i<=n;i++)
+{
+for(j=i;j<=n;j++)
+{
+if(arr[i]==arr[j])
+System.out.println("duplicate num: "+arr[i]);
+}
+}
+}
+}
