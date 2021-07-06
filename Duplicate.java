3694ed4a6fcc5345434b32010f216b07a936c0e9
Pragna-Deepu <86730768+Pragna-Deepu@users.noreply.github.com>
2021-07-06 07:07:42 +0530

    Add files via upload

--- Duplicate.java
+++ Duplicate.java
@@ -13,9 +13,9 @@ for( i=0;i<n;i++)
 {
 arr[i]=sc.nextInt();
 }
-for(i=0;i<=n;i++)
+for(i=0;i<n;i++)
 {
-for(j=i;j<=n;j++)
+for(j=i+1;j<n;j++)
 {
 if(arr[i]==arr[j])
 System.out.println("duplicate num: "+arr[i]);
